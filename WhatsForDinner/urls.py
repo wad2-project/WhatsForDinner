8a6789ad8c0526b91c39354e9e8286e6b9761f8a
Mingchen Sun <2397828s@student.gla.ac.uk>
2019-03-13 23:17:46 +0000

    Models & Population

--- WhatsForDinner/urls.py
+++ WhatsForDinner/urls.py
@@ -17,9 +17,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from whats_for_dinner import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^whatsfordinner/', include('whats_for_dinner.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
