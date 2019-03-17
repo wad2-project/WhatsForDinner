0f1f165c2eb3bc4da6d4a88981d58ae3c8921815
Yash Chakrabarti <yashchakrabarti@gmail.com>
2019-03-17 08:22:57 +0000

    Fixes after merge

--- whats_for_dinner/urls.py
+++ whats_for_dinner/urls.py
@@ -3,6 +3,6 @@ from whats_for_dinner import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^$/', views.about, name='about'),
-    url(r'^$/', views.result, name='result'),
+    url(r'^$', views.about, name='about'),
+    url(r'^$', views.result, name='result'),
 ]
